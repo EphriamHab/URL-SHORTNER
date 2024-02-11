71ad33635bec88062b0c4157fcd12faa0003c97f
EphriamHab <ephremhabtamu2015@gmail.com>
2024-02-11 15:42:29 +0300

    index.js modified

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-import express from 'express'
-const {connectTOMongodb} = require('./connect')
-const urlRoute = require('./routes/url')
-const app = express()
-const PORT = 8001
+import express from 'express';
+import { connectToMongodb } from './connect.js';
+import urlRoute from './routes/url.js';
+import URL from "./models/Url.js";
+import router from './routes/url.js';
 
-app.use('/url', urlRoute)
 
-app.listen(PORT, ()=>console.log(`server started at PORT:${PORT}`))
+const app = express();
+const PORT = 8001;
+
+connectToMongodb(`mongodb://127.0.0.1:27017/short-url`)
+    .then(() => console.log("Mongodb connected"));
+
+
+app.use(express.json())
+app.use('/url', urlRoute);
+
+app.get('/:shortId', async(req,res)=>{
+ const shortId = req.params.shortId
+ const entry = await URL.findOneAndUpdate({
+    shortId
+ },{$push:{
+    visitHistory: {
+        timestamp: Date.now()
+    }
+ },
+}
+);
+res.redirect(entry.redirectURL)
+})
+
+
+app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
