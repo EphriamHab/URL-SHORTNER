695dbc4dab755e4f6f1bb9530138f76ef723b72e
EphriamHab <ephremhabtamu2015@gmail.com>
2024-02-11 15:44:29 +0300

    controllers modified

--- controllers/url.js
+++ controllers/url.js
@@ -1,17 +1,21 @@
-import {nanoid} from 'nanoid'
-const URL = require('./model/url.js');
-async function handleGenerateNewShortURL(req, res){
-   const body = req.body
-   if(!body.url) return res.status(400).json({error:'Url required'})
-   const shortID = nanoid(8);
-   await URL.create({
-    shortId: shortID,
-    redirectURL: body.url,
-    visitHistory:[],
-   });
-   return res.json({id: shortID})
+
+import { nanoid } from 'nanoid';
+import URL from "../models/Url.js";
+
+export async function handleGenerateNewShortURL(req, res) {
+    const body = req.body;
+    if (!body.url) return res.status(400).json({ error: 'Url required' });
+    const shortID = nanoid(8);
+    await URL.create({
+        shortId: shortID,
+        redirectURL: body.url,
+        visitHistory: [],
+    });
+    return res.json({ id: shortID });
 }
 
-module.exports ={
-    handleGenerateNewShortURL,
+export async function hadleAnalytics(req, res){
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({shortId})
+  return res.json({totalClicks: result.visitHistory.length })
 }
